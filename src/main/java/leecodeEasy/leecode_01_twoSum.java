54fd5d83e1145d346d1d43099e15be8a506b300e
zengjf <zengjf@jiguang.cn>
2020-03-10 21:56:03 +0800

    first

---
+++ src/main/java/leecodeEasy/leecode_01_twoSum.java
@@ -0,0 +1,27 @@
+package leecodeEasy;
+
+import java.util.HashMap;
+
+/**
+ * Author:Jude
+ * Date:2020-03-10 8:47 AM
+ */
+class Solution{
+    public int[] twoSum(int[] nums,int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[j] == target - nums[i]) {
+
+                    return new int[]{i, j};
+                }
+
+            }
+
+        }
+        throw new IllegalArgumentException("no Two sum solution");
+
+    }
+}
---
+++ src/main/java/leecodeEasy/leecode_02_reverse.java
@@ -0,0 +1,8 @@
+package leecodeEasy;
+
+/**
+ * Author:Jude
+ * Date:2020-03-10 9:14 AM
+ */
+public class leecode_02_reverse {
+}
