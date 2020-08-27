261f8254a1570b126264cda35fdb3579f6825af6
zengjf <270806204@qq.com>
2020-08-27 11:08:54 +0800

    leetcode0215

---
+++ leetcode/src/main/java/lc_0215_findKthLargest.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+/**
+ * Author:Jude
+ * Date:2020-08-27 10:56 上午
+ */
+public class lc_0215_findKthLargest {
+    public static void main(String[] args) {
+        findKthLargestSolution findKthLargestSolution = new findKthLargestSolution();
+
+        //初始化数组
+        int [] numbers = {3,2,1,5,6,4};
+        int kthLargest = findKthLargestSolution.findKthLargest(numbers, 2);
+        System.out.println(kthLargest);
+    }
+
+}
+
+// 方法一  排序 ：时间复杂度 O(NlogN)，空间复杂度 O(1)
+class findKthLargestSolution{
+    public int findKthLargest(int[] numbers,int k){
+
+        Arrays.sort(numbers);
+        return numbers[numbers.length -k];
+
+    }
+}
